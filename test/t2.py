425010b0a7e127ec80d580034b5e316e0466861c
zhang1998 <2764207312@qq.com>
2018-11-14 23:03:30 +0800

    弄不明白的提交

--- test/t2.py
+++ test/t2.py
@@ -1,7 +1,13 @@
-#读取一幅图像，并从图像的两个矩形区域估算出类概率,然后创建一个图:
 
+# -*- coding: utf-8 -*-
+from PIL import Image
+import PIL
+from numpy.ma import array, zeros
+import matplotlib.pyplot
 from scipy.misc import imresize
 import  graphcut
+# 读取一幅图像，并从图像的两个矩形区域估算出类概率,然后创建一个图:
+
 im = array(Image.open('empire.jpg'))
 im = imresize(im,0.07,interp='bilinear')
 size = im.shape[:2]
@@ -13,6 +19,7 @@ labels[-18:-3,-18:-3] = 1
 g = graphcut.build_bayes_graph(im,labels,kappa=1)
 # 对图进行分割
 res = graphcut.cut_graph(g,size)
+
 figure()
 graphcut.show_labeling(im,labels)
 figure()
